ea10c2e23d46b0eba045d6e82844a31056d1149c
xmodulus <azizsafudin@yahoo.com.sg>
2017-09-09 20:15:41 +1200

    Click event on timestamp caught.

--- content_script.js
+++ content_script.js
@@ -1,10 +1,28 @@
 //Jquery works here
 
+
 // $("abbr.livetimestamp").hide();
+
+// $("abbr.livetimestamp").on('click', '*', function(event) {
+//      window.alert(' ID of element- testing'); 
+//     window.alert(' ID of element=' + $(this).attr('id'));  // Get ID attribute
+//     window.alert(' ID of Parent element=' + $(this).parent().attr('id'));
+// });
 
 $('abbr.livetimestamp').on('click', function(e){
 	e.preventDefault();
-alert($(this).parent().attr('href'));
+	var url = "https://www.facebook.com"+$(this).parent("a").attr('href')
+	alert(url);
+
+	// $.ajax({
+	// type: "POST",
+	// url: "http://188.166.214.237/api/v1/sendPost",
+	// data: data,
+	// success: success,
+	// dataType: "JSON"
+	// });
 });
+
+
 
 alert("RedList Chrome Extension is loaded.");
