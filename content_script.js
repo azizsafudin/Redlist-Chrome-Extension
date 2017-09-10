dfd795036d5bf0a83bc3afdf8506c6a79b435122
xmodulus <azizsafudin@yahoo.com.sg>
2017-09-10 22:17:40 +1200

    Added pageAction, and chrome.storage

--- content_script.js
+++ content_script.js
@@ -3,7 +3,7 @@ var viewportHeight = $(window).height();
 
 $(document).scroll(()=>{
 	var current_scroll_top = $(window).scrollTop();
-    var scroll_delta = current_scroll_top - old_scroll_top;
+  var scroll_delta = current_scroll_top - old_scroll_top;
 
     //if user scrolls more than the current scroll position
 	if (current_scroll_top >= 400+old_scroll_top){
@@ -16,35 +16,66 @@ $(document).scroll(()=>{
 });
 
 $(document).ready(()=>{
-	main();
+
+$("abbr.livetimestamp").prepend("&#9762; ");
+
 })
 
 function main(){
-  	$('abbr.livetimestamp')
-  	.attr("title", "Right click to add to the List!")
-  	.css("font-weight", "bold")
-  	.css("color", "red");
 
-  	$('abbr.livetimestamp').hover(
+    //cosmetics
+  	$("abbr.livetimestamp")
+  	.attr("title", "Right click to Redlist this comment!");
+
+  	$("abbr.livetimestamp").hover(
     function() {
         var $this = $(this); // caching $(this)
-        $this.data('initialText', $this.text());
-        $this.text("Add to the List!");
+        $this.data("initialText", $this.text());
+        $this.css("color", "crimson");
+        $this.css("font-weight", "bold")
+        $this.text("Redlist it!");
     },
     function() {
         var $this = $(this); // caching $(this)
-        $this.text($this.data('initialText'));
+        $this.text($this.data("initialText"));
     }
-);
-    $('abbr.livetimestamp').mousedown(function(e){
+    );
+
+
+    $("abbr.livetimestamp").mousedown(function(e){
         if(e.which == 3){
         e.preventDefault();
-        var url = "https://www.facebook.com"+$(this).parent("a").attr('href');
-        alert(url);
+
+        var url = "https://www.facebook.com"+$(this).parent("a").attr("href");
+        var new_obj = { "full_url": url.toString() }
+        var latest_list = [];
+
+        chrome.storage.sync.get("list", function(items) {
+          latest_list = JSON.parse(items["list"]);
+          console.log(items["list"]);
+        });
+
+          latest_list.push(new_obj);
+          //console.log(latest_list);
+
+        chrome.storage.sync.set({"list": JSON.stringify(latest_list)}, function() {
+        // Notify that we saved.
+          alert("SAVED URL:"+url.toString());
+        });
+        
+        }
+
+        //middle click to get stored item.
+        if(e.which == 2){
+        chrome.storage.sync.get("list", function(items) {
+          alert(items["list"].toString());
+        });
         }       
     });
 
-    //var data = { postURL: url }
+  //setInterval(function(){location.reload(true);}, 60000);
+
+  //var data = { postURL: url }
 
  	// $.ajax({
 	// type: "POST",
@@ -55,4 +86,30 @@ function main(){
 	// },
 	// dataType: "JSON"
 	// });	
+}
+
+
+//Code to reload page when idle.
+var IDLE_TIMEOUT = 60;
+var _idleSecondsCounter = 0;
+document.onclick = function() {
+    _idleSecondsCounter = 0;
+};
+document.onmousemove = function() {
+    _idleSecondsCounter = 0;
+};
+document.onkeypress = function() {
+    _idleSecondsCounter = 0;
+};
+document.onscroll = function() {
+    _idleSecondsCounter = 0;
+};
+
+window.setInterval(CheckIdleTime, 1000);
+
+function CheckIdleTime() {
+    _idleSecondsCounter++;
+    if (_idleSecondsCounter >= IDLE_TIMEOUT) {
+        location.reload(true);
+    }
 }
